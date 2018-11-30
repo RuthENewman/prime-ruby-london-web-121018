f6c050758afe8a2f7fbfa9cd3e75ca406710f95a
ruthnewman <ruthnewman@gmail.com>
2018-11-30 01:25:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(integer) 
+  range = (2..80000)
+  array = range.to_a 
+  newArray = [];
+  if integer < 1
+    return false
+  elsif 
+    array.each do |number|
+    newArray << integer % number
+    newArray.include?(0)
+  end
+end  
+end 
